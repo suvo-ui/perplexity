fc5f4fb9f02afa84a6bcd2be422c96bc5bfd9c29
suvo-ui <rebantabiswas265@gmail,com>
2026-09-25 10:59:05 +0530

    prod push

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,10 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://perplexity-pied-five.vercel.app",
+    ],
     credentials: true,
   }),
 );
