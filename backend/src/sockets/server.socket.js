52575797a403fc6bc7a8fbd6c19875ee095448a8
suvo-ui <rebantabiswas265@gmail,com>
2026-09-25 14:31:31 +0530

    socket issue

--- backend/src/sockets/server.socket.js
+++ backend/src/sockets/server.socket.js
@@ -5,7 +5,7 @@ let io;
 export function initSocket(httpServer) {
   io = new Server(httpServer, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://perplexity-pied-five.vercel.app",
       methods: ["GET", "POST"],
       credentials: true,
     },
